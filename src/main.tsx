73588f95ad740eaae34d2dde7cd3b9cb15fa5fd1
revoltos <mgc.info.new@gmail.com>
2025-12-09 09:57:59 -0300

    feat: set light mode as default and create minimalist dark mode with neutral grays

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { ThemeProvider } from "@/components/theme-provider";
 
 createRoot(document.getElementById("root")!).render(
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
         <App />
     </ThemeProvider>
 );
