db02eb551764344f3bb5c428cf44103ffeb43869
revoltos <mgc.info.new@gmail.com>
2025-12-10 16:45:54 -0300

    fix: tema padrao segue configuracao do sistema

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { ThemeProvider } from "@/components/theme-provider";
 
 createRoot(document.getElementById("root")!).render(
-    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
         <App />
     </ThemeProvider>
 );
