e16313f03b4ba568d9f1533ea8b78f070ceefc28
revoltos <mgc.info.new@gmail.com>
2025-12-29 23:09:13 -0300

    fix: corrige lentidao do accordion no mobile

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -41,7 +41,6 @@ const AccordionContent = React.forwardRef<
   <AccordionPrimitive.Content
     ref={ref}
     className="overflow-hidden text-sm data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
-    style={{ willChange: 'height' }}
     {...props}
   >
     <div className={cn("pb-4 pt-0", className)}>{children}</div>
